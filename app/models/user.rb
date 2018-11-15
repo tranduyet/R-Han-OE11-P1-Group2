ace4b07c867322b10dbeca5a0e6c1f1d82308ac4
nhatvytran <nhatvy2356@gmail.com>
2018-11-15 18:01:36 +0700

    lo

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
           :omniauthable, omniauth_providers: [:google_oauth2]
 
-def self.new_with_session params, session
-    super.tap do |user|
-      if data = session["devise.facebook_data"] &&
-        session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
-
-  def self.from_omniauth(auth)
+def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
